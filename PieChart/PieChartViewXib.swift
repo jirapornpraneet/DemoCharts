868a1baab59ff6105ae3e04b74a643b2b336a083
Nut Jiraporn <nut.j@finnomena.com>
2023-08-30 16:21:34 +0700

    add show chart

--- PieChart/PieChartViewXib.swift
+++ PieChart/PieChartViewXib.swift
@@ -8,9 +8,16 @@
 import UIKit
 import DGCharts
 
-class PieChartViewXib: UIView {
+class PieChartViewXib: UIView, ChartViewDelegate {
 
-    @IBOutlet var chartView: UIView!
+    @IBOutlet var chartView: PieChartView!
+    let parties = ["Party A", "Party B", "Party C", "Party D", "Party E", "Party F",
+                   "Party G", "Party H", "Party I", "Party J", "Party K", "Party L",
+                   "Party M", "Party N", "Party O", "Party P", "Party Q", "Party R",
+                   "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+                   "Party Y", "Party Z"]
+
+    var shouldHideData: Bool = false
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -22,7 +29,72 @@ class PieChartViewXib: UIView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        setupUI()
+        updateChartData()
     }
 
+    func setupUI() {
+        chartView.delegate = self
+
+        let l = chartView.legend
+        l.horizontalAlignment = .right
+        l.verticalAlignment = .top
+        l.orientation = .vertical
+        l.xEntrySpace = 7
+        l.yEntrySpace = 0
+        l.yOffset = 0
+//        chartView.legend = l
+
+        // entry label styling
+        chartView.entryLabelColor = .white
+        chartView.entryLabelFont = .systemFont(ofSize: 12, weight: .light)
+
+        chartView.animate(xAxisDuration: 1.4, easingOption: .easeOutBack)
+    }
+
+    func updateChartData() {
+        if self.shouldHideData {
+            chartView.data = nil
+            return
+        }
+
+        self.setDataCount(Int(10), range: UInt32(20))
+    }
+
+    func setDataCount(_ count: Int, range: UInt32) {
+        let entries = (0..<count).map { (i) -> PieChartDataEntry in
+            // IMPORTANT: In a PieChart, no values (Entry) should have the same xIndex (even if from different DataSets), since no values can be drawn above each other.
+            return PieChartDataEntry(value: Double(arc4random_uniform(range) + range / 5),
+                                     label: parties[i % parties.count]) //i%26
+        }
+
+
+        let set = PieChartDataSet(entries: entries, label: "Election Results")
+        set.drawIconsEnabled = false
+        set.sliceSpace = 2
+
+
+        set.colors = ChartColorTemplates.vordiplom()
+            + ChartColorTemplates.joyful()
+            + ChartColorTemplates.colorful()
+            + ChartColorTemplates.liberty()
+            + ChartColorTemplates.pastel()
+            + [UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)]
+
+        let data = PieChartData(dataSet: set)
+
+        let pFormatter = NumberFormatter()
+        pFormatter.numberStyle = .percent
+        pFormatter.maximumFractionDigits = 1
+        pFormatter.multiplier = 1
+        pFormatter.percentSymbol = " %"
+        data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
+
+
+        data.setValueFont(UIFont.systemFont(ofSize: 11, weight: .light))
+        data.setValueTextColor(UIColor.black)
+
+        chartView.data = data
+        chartView.highlightValues(nil)
+    }
 }
