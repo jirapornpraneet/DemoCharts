7c1251988012cc64e88a57be00811ddec80331ba
Nut Jiraporn <nut.j@finnomena.com>
2023-08-30 16:32:02 +0700

    remove comment

--- PieChart/PieChartViewXib.swift
+++ PieChart/PieChartViewXib.swift
@@ -19,14 +19,6 @@ class PieChartViewXib: UIView, ChartViewDelegate {
 
     var shouldHideData: Bool = false
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
     override func awakeFromNib() {
         super.awakeFromNib()
         setupUI()
